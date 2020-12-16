96f63b5f21b219a7f72fe137a71b4857949de4c0
stefi23 <stefania.rosca2309@gmail.com>
2020-12-16 18:12:58 +0100

    added styled-components and styled the SkierCard

--- client/src/components/SkierCard.js
+++ client/src/components/SkierCard.js
@@ -1,45 +1,71 @@
-import React from "react"
+import React, {useEffect} from "react"
+import styled, {css} from 'styled-components'
+import axios from "axios";
+
+    const SkierCardDiv = styled.div`
+        background: #fcfdff;
+        padding : 30px 15px;
+        border-radius: 10px;
+        boxShadow: 0px 4px 8px 0px hsla(0, 0%, 0%, 0.2);
+    `
+    const Title = styled.h1`
+        color: #6989af;
+        font-size: 20px;
+        font-weight: bold;
+        margin-bottom: 5px;
+        text-align : center;
+`;
+
+    const LightText = styled.p`
+        text-align : center;
+        font-size: 14px;
+        color: hsl(214, 7%, 47%);
+    `
+
+    const StyledP = styled.p`
+        font-size: 14px;
+        color: #001c00;
+    `
+
+    const TextCenter = styled.h1`
+        text-align:center;
+    `
 
 function SkierCard(){
 
-    const skierCard = {
-        "border-radius": "10px",
-        "padding" : "30px 15px",
-        "boxShadow": "0px 4px 8px 0px hsla(0, 0%, 0%, 0.2)",
-        "backgroundColor": "#fcfdff"
-      
-        
-    }
-
-    const title = {
-        "color": "#6989af",
-        "fontSize": "20px",
-        "fontWeight": "bold",
-        "margin-bottom":"5px",
-        "textAlign" : "center"
-    }
-
-    const lightText = {
-        "fontSize": "14px",
-        "color": "hsl(214, 7%, 47%",
-        "textAlign" : "center"
-    }
-
-    const pText = {
-        "fontSize": "14px",
-        "color": "#001c00"
-
-    }
-
     return(
-        <div style={skierCard}>
-            <h1 style={ {"textAlign" : "center"}}>⛷️</h1>
-            <p style={title}>Stefi Rosca</p>
-            <p style ={lightText}> - pro - </p>
-            <p style={pText}>🏔️ &nbsp;Andorra, La Molina</p>
-            <p style={pText}>🗣️ &nbsp;English, Spanish, German, Romanian</p>
-        </div>
+        <SkierCardDiv>
+         
+            <TextCenter>⛷️</TextCenter>
+            <Title>Stefi Rosca</Title>
+            <LightText>- pro -</LightText>
+            <StyledP>🏔️ &nbsp;Andorra, La Molina</StyledP>
+            <StyledP>🗣️ &nbsp;English, Spanish, German, Romanian</StyledP>
+          
+        </SkierCardDiv>
     )
 }
 
 export default SkierCard
+
+
+      
+
+
+
+
+
+//     return(
+//         <SkierCardDiv>
+   
+//             <h1 style={ {"textAlign" : "center"}}>⛷️</h1>
+//             <p style={title}>Stefi Rosca</p>
+//             <p style ={lightText}> - pro - </p>
+//             <p style={pText}>🏔️ &nbsp;Andorra, La Molina</p>
+//             <p style={pText}>🗣️ &nbsp;English, Spanish, German, Romanian</p>
+    
+//         </SkierCardDiv>
+//     )
+// }
+
+// export default SkierCard
