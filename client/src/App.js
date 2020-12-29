d07f08ed27de2543ac59a5a13853acc6debbb02c
stefi23 <stefania.rosca2309@gmail.com>
2020-12-29 17:43:56 +0100

    updated fetch request to try catch syntax

--- client/src/App.js
+++ client/src/App.js
@@ -18,22 +18,27 @@ function App() {
   const [isUserLoggedin, setUserLoggedIn] = useState(false);
   const [name, getName] = useState("");
 
+  const getUserdatafromDB = async () => {
+    try {
+      const resp = await axios.get('/users/profile', {
+        headers: {
+          "x-access-token": localStorage.getItem("skiBuddyToken")
+        }
+      }
+      );
+      getName(resp.data.name);
+      setUserLoggedIn(true);
+    } catch (err) {
+      // Handle Error Here
+      console.error("User is not loged in");
+    }
+  }
+
   useEffect(() => {
-    axios("/users/profile", {
-      method: "GET",
-      headers: {
-        "x-access-token": localStorage.getItem("skiBuddyToken"),
-      },
-    })
-      .then((result) => {
-        console.log(result.data);
-        getName(result.data.name);
-        setUserLoggedIn(true);
-      })
-      .catch((error) => console.log(error));
+    getUserdatafromDB()
   }, []);
 
-  function handleLogout(){
+  function handleLogout() {
     setUserLoggedIn(false);
     window.localStorage.removeItem('skiBuddyToken');
   }
@@ -42,81 +47,81 @@ function App() {
     <div className="container">
       <div className="row">
         <div className="col-md-12">
-    <Router>
-        <Navbar bg="dark" variant="dark" className="mt-3 mb-3">
-          <Navbar.Brand href="/">Ski Buddies</Navbar.Brand>
-          <Navbar.Toggle />
-          <Navbar.Collapse className="justify-content-end">
-            {isUserLoggedin ? (
-              <ul className="navbar-nav">
-                <li className="nav-item">
-              <Navbar.Text>
-                Welcome {name} 
-                </Navbar.Text>
-                </li>
-                 <li className="nav-item">
-                <Link to="/" 
-                      className="nav-link"
-                      style={{'color': '#e2e2e5'}}
-                      onClick={handleLogout}>Log out
-                </Link> 
-                </li>
-                </ul>
-              
-              
-            ) : (
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <Link to="/login" 
+          <Router>
+            <Navbar bg="dark" variant="dark" className="mt-3 mb-3">
+              <Navbar.Brand href="/">Ski Buddies</Navbar.Brand>
+              <Navbar.Toggle />
+              <Navbar.Collapse className="justify-content-end">
+                {isUserLoggedin ? (
+                  <ul className="navbar-nav">
+                    <li className="nav-item">
+                      <Navbar.Text>
+                        Welcome {name}
+                      </Navbar.Text>
+                    </li>
+                    <li className="nav-item">
+                      <Link to="/"
                         className="nav-link"
-                        style={{'color': '#e2e2e5'}}>
-                    Login
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link
-                    to="/register"
-                    className="nav-link"
-                    style={{'color': '#e2e2e5'}}
-                    // data-toggle="modal"
-                    // data-target="#exampleModalLong"
-                  >
-                    Sign Up
-                  </Link>
-                </li>
-              </ul>
-            )}
-          </Navbar.Collapse>
-        </Navbar>
+                        style={{ 'color': '#e2e2e5' }}
+                        onClick={handleLogout}>Log out
+                </Link>
+                    </li>
+                  </ul>
 
-        <Switch>
-          <Route path="/login">
-            {isUserLoggedin ? (
-              <Redirect to="/" />
-            ) : (
-              <Login updateLoggedIn={setUserLoggedIn} getName={getName} />
-            )}
-          </Route>
-          <Route path="/register">
-            {<Register updateLoggedIn={setUserLoggedIn} />}
-          </Route>
-          <Route path="/"></Route>
-        </Switch>
 
-    </Router>
-       </div>
-          </div>   
-              <div className="row">
-                 <div className="col-sm-12 col-md-3 col-lg-3 col-xl-3 mb-12">
-                <Search />
-                </div>
-                <div className="col-sm-12 col-md-9 col-lg-9 col-xl-9 mb-12">
-                  <div className="row">
-                <SkiersList isUserLoggedin={isUserLoggedin}/>
-                </div>
-                </div>
-              </div>
-  </div>
+                ) : (
+                    <ul className="navbar-nav">
+                      <li className="nav-item">
+                        <Link to="/login"
+                          className="nav-link"
+                          style={{ 'color': '#e2e2e5' }}>
+                          Login
+                  </Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link
+                          to="/register"
+                          className="nav-link"
+                          style={{ 'color': '#e2e2e5' }}
+                        // data-toggle="modal"
+                        // data-target="#exampleModalLong"
+                        >
+                          Sign Up
+                  </Link>
+                      </li>
+                    </ul>
+                  )}
+              </Navbar.Collapse>
+            </Navbar>
+
+            <Switch>
+              <Route path="/login">
+                {isUserLoggedin ? (
+                  <Redirect to="/" />
+                ) : (
+                    <Login updateLoggedIn={setUserLoggedIn} getName={getName} />
+                  )}
+              </Route>
+              <Route path="/register">
+                {<Register updateLoggedIn={setUserLoggedIn} />}
+              </Route>
+              <Route path="/"></Route>
+            </Switch>
+
+          </Router>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-sm-12 col-md-3 col-lg-3 col-xl-3 mb-12">
+          <Search />
+        </div>
+        <div className="col-sm-12 col-md-9 col-lg-9 col-xl-9 mb-12">
+          <div className="row">
+            <SkiersList isUserLoggedin={isUserLoggedin} />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
