fceaacd4241db1bead9bdb3d06ce82530fe3d63e
stefi23 <stefania.rosca2309@gmail.com>
2021-01-10 17:16:02 +0200

    style register, updated login style

--- client/src/components/RadioBox.js
+++ client/src/components/RadioBox.js
@@ -1,24 +1,24 @@
 import React from "react";
 
-function RadioBox({label,value,onChange, defaultChecked, id, name}){
-    return(
-    <> 
-         <input
-            className="form-check-input"
-            type="radio"
-            name={name}
-            id="inlineRadio1"
-            value={value}
-            onChange={onChange}
-            defaultChecked={defaultChecked}
-            id={id}
-        />
-        <label className="form-check-label" for={id}>
-            {label}
-        </label>
+function RadioBox({ label, value, onChange, defaultChecked, id, name }) {
+    return (
+        <>
+            <input
+                className="form-check-input"
+                type="radio"
+                name={name}
+                id="inlineRadio1"
+                value={value}
+                onChange={onChange}
+                defaultChecked={defaultChecked}
+                id={id}
+            />
+            <label className="text-gray form-check-label" for={id}>
+                {label}
+            </label>
         </>
 
-       
+
     )
 }
 
