2934b73eb11b1e81ff4d4186488a035d40ba5156
stefi23 <stefania.rosca2309@gmail.com>
2021-01-18 23:38:19 +0200

    added prpTypes and renamed variables

--- client/src/components/Dropbox.js
+++ client/src/components/Dropbox.js
@@ -1,50 +1,58 @@
 import React, { useState, useRef } from "react"
-import propTypes from 'prop-types';
+import PropTypes from 'prop-types';
+
 
 Dropbox.propTypes = {
-    value: propTypes.string.isRequired
+    value: PropTypes.string.isRequired,
+    suggestions: PropTypes.arrayOf(PropTypes.string).isRequired,
+    title: PropTypes.string.isRequired,
+    placeholder: PropTypes.string.isRequired,
+    onClick: PropTypes.func.isRequired,
+    onFilter: PropTypes.func.isRequired
 }
 
-function Dropbox({ onFilter, onClick, placeholder, data, value }) {
+
+
+function Dropbox({ onFilter, onClick, placeholder, suggestions, value, title }) {
     const [openSuggestions, setOpenSuggestions] = useState(false)
-    const [activeSuggestion, setActiveSuggestion] = useState(-1)
+    const [activeSuggestionIndex, setactiveSuggestionIndex] = useState(-1)
 
     const containerRef = useRef(null);
 
     const onKeyDown = (e) => {
         if (e.keyCode === 13) {
             //13 = ENTER KEY
-            onFilter(data[activeSuggestion])
+            onFilter(suggestions[activeSuggestionIndex])
             setOpenSuggestions(false)
-            setActiveSuggestion(0)
+            setactiveSuggestionIndex(0)
 
         } else if (e.keyCode === 38) {
             // 38 = ARROW UP
-            if (activeSuggestion === 0) {
+            if (activeSuggestionIndex === 0) {
                 return
             }
-            setActiveSuggestion(activeSuggestion - 1)
+            setactiveSuggestionIndex(activeSuggestionIndex - 1)
 
             containerRef.current.children.length &&
-                containerRef.current.children[activeSuggestion - 1].scrollIntoView(false);;
+                containerRef.current.children[activeSuggestionIndex - 1].scrollIntoView(false);;
         }
         else if (e.keyCode === 40) {
             // 40 = ARROW DOWN
             setOpenSuggestions(true)
-            if (activeSuggestion === data.length - 1) {
+            if (activeSuggestionIndex === suggestions.length - 1) {
                 return
             }
-            setActiveSuggestion(activeSuggestion + 2)
-            setActiveSuggestion(activeSuggestion + 1);
+            // setactiveSuggestionIndex(activeSuggestionIndex + 2)
+            setactiveSuggestionIndex(activeSuggestionIndex + 1);
             containerRef.current.children.length &&
-                containerRef.current.children[activeSuggestion + 1].scrollIntoView(false);
+                containerRef.current.children[activeSuggestionIndex + 1].scrollIntoView(false);
 
         }
         else if ((e.keyCode === 8 && e.target.value === "") || e.keyCode === 27) {
             setOpenSuggestions(false)
         }
         else {
-            setActiveSuggestion(0);
+            setactiveSuggestionIndex(0);
             containerRef.current.children.length &&
                 containerRef.current.children[0].scrollIntoView(false);
         }
@@ -54,7 +62,7 @@ function Dropbox({ onFilter, onClick, placeholder, data, value }) {
     return (
 
         <>
-            <label>Which resort?</label>
+            <label>Which {title}?</label>
 
             <input
                 className="form-control input"
@@ -75,17 +83,17 @@ function Dropbox({ onFilter, onClick, placeholder, data, value }) {
                 <div className="parent">
                     <div className="autocomplete rounded" ref={containerRef}   >
 
-                        {data.length > 0 && openSuggestions ? (
+                        {suggestions.length > 0 && openSuggestions ? (
 
-                            data.map((resort, index) => (
+                            suggestions.map((resort, index) => (
                                 <p
                                     tabIndex="0"
                                     key={index}
-                                    className={index === activeSuggestion ? "autosuggestElement-active p-2 mb-0" : "autosuggestElement p-2 mb-0"}
+                                    className={index === activeSuggestionIndex ? "autosuggestElement-active p-2 mb-0" : "autosuggestElement p-2 mb-0"}
                                     onMouseDown={() =>
                                         onFilter(resort)
                                     }
-                                    onMouseMove={() => setActiveSuggestion(index)}
+                                    onMouseMove={() => setactiveSuggestionIndex(index)}
                                     value={resort}
                                 >
                                     {resort}
